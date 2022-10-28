e20eb3ae4a3aa5389edd4226a2a2d3cd8a23ccad
Eugene Velesevich <eugvelesevich@gmail.com>
2022-10-28 13:12:45 +0300

    Rewrite user js script
    Implement a wrapper for $.ajax to capture chat message request

--- godchattotlgrm.user.js
+++ godchattotlgrm.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Chat to Telegram
 // @namespace    https://godville.net/
-// @version      0.3
+// @version      0.4
 // @description  Send chat messages to the telegram channel
 // @author       Belka
 // @match        https://godville.net/superhero
 // @grant        GM_xmlhttpRequest
 // @run-at       document-idle
-// @connect      46.229.213.219
+// @connect      <server url>
 // ==/UserScript==
 
 (function() {
@@ -23,7 +23,6 @@
       var body = "token=" + encodeURIComponent(userToken) +
                  "&text=" + encodeURIComponent(textmessage);
 
-      console.log(body)
       var makeRequest = function() {
         GM_xmlhttpRequest({method: "POST",
           url: ServerIP,
@@ -49,51 +48,32 @@
     }
   };
   setTimeout(sendMessages, 3000)
-  var sendToBot = function(time, user, message) {
-    var d = new Date();
-    var curHour = d.getHours();
-    var curMinute = d.getMinutes();
-    var parsedMesTime = mesTimespampRegex.exec(time);
-    if (parsedMesTime === null) {
-      return true;
-    }
-    var mesDate;
-    if (parsedMesTime[1] == undefined) {
-      var extday = 0;
-      if (curHour < +parsedMesTime[4] || (curHour == +parsedMesTime[4] && curMinute < +parsedMesTime[5])) {
-        extday = -1;
-      }
-      mesDate = new Date(d.getFullYear(), d.getMonth(), d.getDate()+extday, +parsedMesTime[4], +parsedMesTime[5]);
-    }
-    else {
-      mesDate = new Date(2000 + +parsedMesTime[3], +parsedMesTime[2], +parsedMesTime[1], +parsedMesTime[4], +parsedMesTime[5]);
-    }
-
-    MessageQueue.push([mesDate.getTime()/1000-mesDate.getTimezoneOffset()*60, user, message]);
+  var lastmid = 0;
+  var sendToBot = function(time, user, message, mid) {
+    if (mid <= lastmid) return;
+    lastmid = mid;
+    var d = new Date(time);
+    MessageQueue.push([d.getTime()/1000-d.getTimezoneOffset()*60, user, message]);
+  };
+  var handleAjaxResult = function(res) {
+    res.msg.forEach(function(e) {
+      sendToBot(e.t, e.u, e.m, e.id);
+    });
   };
   setTimeout(function() {
-    var target = $('.chat_ph');
-
-    var observer = new MutationObserver(function(mutations) {
-      mutations.forEach(function(mutation) {
-        if (typeof mutation.addedNodes == "object") {
-          var next = $(mutation.addedNodes).find('.gc_fr_el').each( function() {
-            var next = $(this);
-            if(next && next.attr("title") && next.attr("title").match(/[0-2]?[0-9]:[0-6][0-9] /)) {
-              var j = next.prev();
-              var message = next.parent().parent().clone().children().remove("*:not(a)").end().text();
-              var user = j.text();
-              var time = next.attr("title");
-              sendToBot(time, user, message);
-            }
-          });
-        }
-      });
-    });
-
-    var config = { childList: true, subtree: true }
-
-    observer.observe(target.get(0), config);
+    var originAjax = $.ajax;
+    $.ajax = function(a,b) {
+      if (a.url.includes('MRsaFLGpTaPol6FFhAtS') ||
+          (Object.hasOwn(a.data, 'a') &&
+           a.data.a.includes('3GGcSQqPm4DC1ynsBr8l'))) {
+        var originSuccess = a.success;
+        a.success = function(n) {
+          originSuccess(n);
+          handleAjaxResult(n);
+        };
+      }
+      originAjax(a,b);
+    };
   },1000);
 })();
 
